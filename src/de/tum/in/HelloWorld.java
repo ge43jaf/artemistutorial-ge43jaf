ca841c1acfb447ad4b9b00f5c7d996fcef25eb06
ge43jaf <73897542+ge43jaf@users.noreply.github.com>
2020-11-03 23:24:25 +0800

    Update HelloWorld.java
    Change the text "Hello World" to "Hello Artemis"

--- src/de/tum/in/HelloWorld.java
+++ src/de/tum/in/HelloWorld.java
@@ -2,11 +2,11 @@ package de.tum.in;
 
 public class HelloWorld {
     
-    // TODO Change the text "Hello World" to "Hello Artemis"
+    // DONE Change the text "Hello World" to "Hello Artemis"
 
     public static void main(String[] args) {
         // Prints "Hello World" to the terminal window.
-        System.out.println("Hello World");
+        System.out.println("Hello Artemis");
     }
 
 }
